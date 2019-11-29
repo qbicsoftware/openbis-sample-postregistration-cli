e2ee92a6f33a4f27c38c292162a4df4320566a75
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-11-29 10:44:12 +0100

    Includes debugging statement for request

--- sample-status-updater-infrastructure/src/main/groovy/life/qbic/samplestatusupdater/update/SampleTrackingConnector.groovy
+++ sample-status-updater-infrastructure/src/main/groovy/life/qbic/samplestatusupdater/update/SampleTrackingConnector.groovy
@@ -1,5 +1,6 @@
 package life.qbic.samplestatusupdater.update
 
+import groovy.util.logging.Log4j2
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.client.HttpClient
@@ -8,6 +9,7 @@ import life.qbic.datamodel.services.Location
 import life.qbic.samplestatusupdater.ServiceUserCredentials
 import life.qbic.services.Service
 
+@Log4j2
 class SampleTrackingConnector implements SampleTrackingService {
 
     ServiceUserCredentials credentials
@@ -24,7 +26,7 @@ class SampleTrackingConnector implements SampleTrackingService {
         HttpClient client = RxHttpClient.create(service.rootUrl)
         //TODO this is only a workaround, as the client seems not to prepend the base url.
         URI sampleUri = new URI("${service.rootUrl.toExternalForm()}/samples/$sampleCode/currentLocation/")
-
+        log.debug("final uri is $sampleUri")
         HttpRequest request = HttpRequest.POST(sampleUri, location).basicAuth(credentials.name, credentials.pw)
         client.withCloseable {
             def response = it.toBlocking().exchange(request)
