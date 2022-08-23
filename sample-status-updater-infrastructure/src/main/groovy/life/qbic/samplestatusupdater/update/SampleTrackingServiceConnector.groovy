e2269b7323f8adafb15625e51bdb465752d1d597
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-08-23 15:47:17 +0200

    Revert "add space to trigger GitHub Action"
    This reverts commit e0c806eb39bce47fec8fd5c380149fac9ba2435c.

--- sample-status-updater-infrastructure/src/main/groovy/life/qbic/samplestatusupdater/update/SampleTrackingServiceConnector.groovy
+++ sample-status-updater-infrastructure/src/main/groovy/life/qbic/samplestatusupdater/update/SampleTrackingServiceConnector.groovy
@@ -40,7 +40,6 @@ class SampleTrackingServiceConnector implements SampleTrackingService {
     @Override
     def registerFirstSampleLocation(String sampleCode, Location location) throws SampleUpdateException {
         HttpClient client = RxHttpClient.create(service.rootUrl)
-        //test
         //TODO this is only a workaround, as the client seems not to prepend the base url.
         URI sampleUri = new URI("${service.rootUrl.toExternalForm()}/samples/$sampleCode/")
 
