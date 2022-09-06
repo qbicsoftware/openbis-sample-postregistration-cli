ca2f24fb62217782d0f6c62adc53eef4a44a6965
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-09-06 14:02:00 +0200

    Log exceptions when they occur.

--- sample-status-updater-domain/src/main/groovy/life/qbic/samplestatusupdater/update/UpdateResponse.groovy
+++ sample-status-updater-domain/src/main/groovy/life/qbic/samplestatusupdater/update/UpdateResponse.groovy
@@ -6,4 +6,12 @@ class UpdateResponse {
 
     boolean updateSuccessful
 
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", UpdateResponse.class.getSimpleName() + "[", "]")
+                .add("sampleCode='" + sampleCode + "'")
+                .add("updateSuccessful=" + updateSuccessful)
+                .toString();
+    }
 }
